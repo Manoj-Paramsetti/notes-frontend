94122851ab21f41af808e26f7d32d3b0124c9eea
Manoj Paramsetti <paramsetti.manoj@gmail.com>
2024-01-02 10:52:29 +0530

    add login route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import ListNotes from './routes/ListNotes';
 import Note from './routes/Note';
 import Bin from './routes/Bin';
 import Page404 from './routes/404';
+import Login from './routes/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<ListNotes />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/note/:id" element={<Note />} />
         <Route path="/bin" element={<Bin />} />
         <Route path="/404" element={<Page404 />} />
